c0db950d57b8bbfd8d25c20b2c4ae9f2c97907d6
TERENYANA <yanayusiv6447@gmail.com>
2025-04-04 16:25:14 +0200

    add CI workflof/ci test

--- code/server/vite.config.ts
+++ code/server/vite.config.ts
@@ -1,12 +1,13 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import dotenv from "dotenv";
 export default defineConfig(({ mode }) => {
-		//charger le fishier .env.test
-		dotenv.config({ path: ".env.test" });
+	//charger le fishier .env.test
+	dotenv.config({ path: ".env.test" });
 
-		//utiliser le serveur MySQL de Github Actions
-		if(process.env.GITHUB_ACTIONS){
-			process.env.MYSQL_HOST="127.0.0.1";
-		}
-        return{};
-	});
+	//utiliser le serveur MySQL de Github Actions
+	if (process.env.GITHUB_ACTIONS) {
+		process.env.MYSQL_HOST = "127.0.0.1";
+		process.env.MYSQL_PASSWORD = "root";
+	}
+	return {};
+});
