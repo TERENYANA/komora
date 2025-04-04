56244794b3a8e36d0b3dbb73df23b7da395a8570
TERENYANA <yanayusiv6447@gmail.com>
2025-04-04 15:52:51 +0200

    add CI workflof/ci test

--- code/client/src/page/HomePage.tsx
+++ code/client/src/page/HomePage.tsx
@@ -1,17 +1,17 @@
 import Notice from "@/component/common/Notice";
 import HeroSection from "../component/client/HeroSection";
-import styles from "/app/src/assets/css/user/home.module.css";
-import Autoslider from "../component/client/Autoslider"
+import styles from "../assets/css/user/home.module.css";
+import Autoslider from "../component/client/Autoslider";
 const HomePage = () => {
 	//Fragment:balisee anonyme <></>
 	return (
-	<main>
+		<main>
 			<div className={styles.homeBackground}>
-				<Notice/>
-			<HeroSection/>
-			<Autoslider/>
-		</div>
-	</main>
+				<Notice />
+				<HeroSection />
+				<Autoslider />
+			</div>
+		</main>
 	);
 };
 export default HomePage;
